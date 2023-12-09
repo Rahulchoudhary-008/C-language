e3489b5ad96419adee6f0359ae839b9522c79496
Rahulchoudhary-008 <153347379+Rahulchoudhary-008@users.noreply.github.com>
2023-12-09 12:03:45 +0530

    Add files via upload

---
+++ about my self.c
@@ -0,0 +1,9 @@
+// abount my self//
+#include<stdio.h>
+int main()
+{
+	printf("I am Rahul Now, am studing in UIET\n");
+	printf("Currently I am completing my B.E degree in information techonlogy\n");
+	printf("I leave in Boys Hostel 8 of UIET. It is in sector 25\n");
+	return 0;
+}
