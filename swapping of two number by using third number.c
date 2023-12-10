fb112af486d4952ab03169a6794d387a1c2f5d8d
Rahulchoudhary-008 <153347379+Rahulchoudhary-008@users.noreply.github.com>
2023-12-10 12:44:37 +0530

    Add files via upload

---
+++ swapping of two number by using third number.c
@@ -0,0 +1,13 @@
+//swapping to two number by using third variable//
+#include<stdio.h>
+int main()
+{
+	int i,j,k;
+	printf("enter the first numbers and then second number:\n");
+	scanf("%d%d",&i,&j);
+//	k=i;
+//	i=j;
+//	j=k;
+	printf("the first number after swapping is:%d\nthe second number after swapping is:%d",j,i);
+	return 0;
+}
