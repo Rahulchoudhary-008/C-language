d16d173a011c2aaee74be761cee01028edcdf52c
Rahulchoudhary-008 <153347379+Rahulchoudhary-008@users.noreply.github.com>
2023-12-10 12:42:18 +0530

    Add files via upload

---
+++ sum to number.c
@@ -0,0 +1,11 @@
+//sum of two number by user//
+#include<stdio.h>
+int main()
+{
+	int sum,i,j;
+	printf("Enter the numbers:");
+	scanf("%d%d",&i,&j);
+	sum=i+j;
+	printf("sum is=%d",sum);
+	return 0;
+}
