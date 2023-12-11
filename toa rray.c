bbafc3e1f88fbe647fb01b71ffee2a3445ae5b16
Rahulchoudhary-008 <153347379+Rahulchoudhary-008@users.noreply.github.com>
2023-12-11 12:35:52 +0530

    pointer to an array.c

--- toa rray.c
+++ toa rray.c
@@ -2,19 +2,20 @@
 #include<stdio.h>
 int main()
 {
+	int i;
 	int a[5]={10,20,30,40,50};
 	int *p;
 	p=a;
-	for (i=0;i<=5;i++)
+	for(i=0;i<=5;i++)
 	{
-		printf("%d",a[i]);
+		printf("%d\n",a[i]);
 	}
-	for (i=0;i<5;i++);
+	for(i=0;i<5;i++);
 	{
-		printf("%d",*p+i);
+		printf("%d\n",*p+i);
 	}
-    for (i=0;i<5;i++)
+    for(i=0;i<5;i++)
     {
-    	printf("%d",p+i);
+    	printf("%d\n",p+i);
 	}
 }
